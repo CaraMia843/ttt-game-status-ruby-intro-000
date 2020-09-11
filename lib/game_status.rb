b196e4eca99f42a7636719a551bc3e22819004c3
Cara Zellner <carajeanine@gmail.com>
2020-09-11 21:35:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,15 +15,5 @@ WIN_COMBINATIONS = [
 [6,4,2]
 ]
 
-def won?(array)
-  WIN_COMBINATIONS.each do |winner_set|
-    if array[winner_set[0]] == array[winner_set[1]] &&
-       array[winner_set[1]] == array[winner_set[2]] &&
-       position_taken?(array, winner_set[0])
-       puts "somebody won!"
-       return true 
-       return winner_set
-    end
-  end
+def won?(board)
 end
-puts won?(board)
